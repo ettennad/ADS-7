e3eec70d409be4b3a9c6883a7eeec54f8c917a9e
ettennad <a.derlukova@mail.ru>
2025-05-15 14:56:51 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,54 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+#include <cstdlib>
+
+Train::Train() {
+  start = nullptr;
+  countOp = 0;
+}
+
+Train::~Train() {
+  if (!start) return;
+  Car* current = start->next;
+  while (current != start) {
+    Car* tmp = current;
+    current = current->next;
+    delete tmp;
+  }
+  delete start;
+}
+
+void Train::addCar(bool light) {
+  Car* newCar = new Car{light, nullptr, nullptr};
+  if (!start) {
+    start = newCar;
+    start->next = start;
+    start->prev = start;
+  } else {
+    Car* last = start->prev;
+    last->next = newCar;
+    newCar->prev = last;
+    newCar->next = start;
+    start->prev = newCar;
+  }
+}
+
+int Train::getLength() {
+  Car* current = start;
+  current->light = true;
+  int length = 1;
+  while (true) {
+    countOp++;
+    current = current->next;
+    length++;
+    if (current->light) {
+      break;
+    } else {
+      current->light = true;
+    }
+  }
+  return length;
+}
+int Train::getOpCount() {
+  return countOp;
+}
