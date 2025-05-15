902c3397083f5850bf879a5e2d5859044280977a
ettennad <a.derlukova@mail.ru>
2025-05-15 16:52:41 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -8,7 +8,7 @@ class Train {
     bool light; // состояние лампочки
     Car *next;
     Car *prev;
-    Car(bool light = false) : light(light), next(nullptr), prev(nullptr) {}
+    explicit Car(bool light = false) : light(light), next(nullptr), prev(nullptr) {}
   };
   int countOp; // счетчик шагов (число переходов из вагона в вагон)
   Car *first; // точка входа в поезд (первый вагон)
