dddfaafbaf0aa0ad1b45a6fcb5fde66550983ca2
ettennad <a.derlukova@mail.ru>
2025-05-15 16:42:59 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,46 +1,49 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
-Train::Train() : countOp(0), first(nullptr) {}
+#include <iostream>
+
+Train::Train() : first(nullptr), countOp(0) {}
+
+Train::~Train() {
+  if (!first) return;
+  Car* cur = first->next;
+  while (cur != first) {
+    Car* toDelete = cur;
+    cur = cur->next;
+    delete toDelete;
+  }
+  delete first;
+}
 
 void Train::addCar(bool light) {
-  Car* newCar = new Car{light, nullptr, nullptr};
-
+  Car* newCar = new Car(light);
   if (!first) {
     first = newCar;
     first->next = first;
     first->prev = first;
   } else {
     Car* last = first->prev;
-    newCar->next = first;
-    newCar->prev = last;
     last->next = newCar;
+    newCar->prev = last;
+    newCar->next = first;
     first->prev = newCar;
   }
 }
 
 int Train::getLength() {
   if (!first) return 0;
-
-  Car* current = first;
-  current->light = true;
-
-  int steps = 1;
-
-  while (true) {
-    countOp++;
+  countOp = 0;
+  bool initialLight = first->light;
+  first->light = !initialLight;
+  Car* current = first->next;
+  countOp++;
+  while (current != first) {
     current = current->next;
-    steps++;
-
-    if (current->light) {
-      break;
-    } else {
-      current->light = true;
-    }
+    countOp++;
   }
-
-  return steps;
+  first->light = initialLight;
+  return countOp;
 }
-
-int Train::getOpCount() {
+int Train::getOpCount() const {
   return countOp;
 }
